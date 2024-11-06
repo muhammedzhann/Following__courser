7c2a48b09bcd750516512072a47ed1987c7c3691
muhammedzhann <muhammedzansatybaldy@37gmail.com>
2024-11-06 14:46:20 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,208 @@
+const image1 = document.getElementById('image1');
+const image2 = document.getElementById('image2');
+const image3 = document.getElementById('image3');
+const image4 = document.getElementById('image4');
+const image5 = document.getElementById('image5');
+const image6 = document.getElementById('image6');
+const image7 = document.getElementById('image7');
+const image8 = document.getElementById('image8');
+const image9 = document.getElementById('image9');
+const originalImageSrc = image2.querySelector('img').src;
+image1.addEventListener('mouseover', () => {
+    image2.querySelector('img').src = 'myphot/IMAGE 2024-11-06 12:18:35.jpg';
+    image3.querySelector('img').src = 'myphot/IMAGE 2024-11-06 12:18:35.jpg';
+    image4.querySelector('img').src = 'myphot/IMAGE 2024-11-06 12:18:24.jpg';
+    image7.querySelector('img').src = 'myphot/IMAGE 2024-11-06 12:18:24.jpg';
+    image5.querySelector('img').src = 'myphot/IMAGE 2024-11-06 12:18:29.jpg';
+    image6.querySelector('img').src = 'myphot/IMAGE 2024-11-06 12:18:29.jpg';
+    image8.querySelector('img').src = 'myphot/IMAGE 2024-11-06 12:18:29.jpg';
+    image9.querySelector('img').src = 'myphot/IMAGE 2024-11-06 12:18:29.jpg';
+});
+image1.addEventListener('mouseout', () => {
+    image2.querySelector('img').src = originalImageSrc;
+    image3.querySelector('img').src = originalImageSrc;
+    image4.querySelector('img').src = originalImageSrc;
+    image7.querySelector('img').src = originalImageSrc;
+    image5.querySelector('img').src = originalImageSrc;
+    image6.querySelector('img').src = originalImageSrc;
+    image8.querySelector('img').src = originalImageSrc;
+    image9.querySelector('img').src = originalImageSrc;
+});
+
+
+image2.addEventListener('mouseover', () => {
+	image1.querySelector('img').src = 'myphot/IMAGE 2024-11-06 12:18:31.jpg';
+	image3.querySelector('img').src = 'myphot/IMAGE 2024-11-06 12:18:35.jpg';
+	image4.querySelector('img').src = 'myphot/IMAGE 2024-11-06 12:18:25.jpg';
+	image7.querySelector('img').src = 'myphot/IMAGE 2024-11-06 12:18:25.jpg';
+	image5.querySelector('img').src = 'myphot/IMAGE 2024-11-06 12:18:24.jpg';
+	image6.querySelector('img').src = 'myphot/IMAGE 2024-11-06 12:18:29.jpg';
+	image8.querySelector('img').src = 'myphot/IMAGE 2024-11-06 12:18:24.jpg';
+	image9.querySelector('img').src = 'myphot/IMAGE 2024-11-06 12:18:29.jpg';
+});
+image2.addEventListener('mouseout', () => {
+	image1.querySelector('img').src = originalImageSrc;
+	image3.querySelector('img').src = originalImageSrc;
+	image4.querySelector('img').src = originalImageSrc;
+	image7.querySelector('img').src = originalImageSrc;
+	image5.querySelector('img').src = originalImageSrc;
+	image6.querySelector('img').src = originalImageSrc;
+	image8.querySelector('img').src = originalImageSrc;
+	image9.querySelector('img').src = originalImageSrc;
+});
+
+ image3.addEventListener('mouseover', () => {
+	image1.querySelector('img').src = 'myphot/IMAGE 2024-11-06 12:18:31.jpg';
+	image2.querySelector('img').src = 'myphot/IMAGE 2024-11-06 12:18:31.jpg';
+	image4.querySelector('img').src = 'myphot/IMAGE 2024-11-06 12:18:25.jpg';
+	image7.querySelector('img').src = 'myphot/IMAGE 2024-11-06 12:18:25.jpg';
+	image5.querySelector('img').src = 'myphot/IMAGE 2024-11-06 12:18:25.jpg';
+	image6.querySelector('img').src = 'myphot/IMAGE 2024-11-06 12:18:24.jpg';
+	image8.querySelector('img').src = 'myphot/IMAGE 2024-11-06 12:18:25.jpg';
+	image9.querySelector('img').src = 'myphot/IMAGE 2024-11-06 12:18:24.jpg';
+ });
+ image3.addEventListener('mouseout', () => {
+	image2.querySelector('img').src = originalImageSrc;
+	image1.querySelector('img').src = originalImageSrc;
+	image3.querySelector('img').src = originalImageSrc;
+	image4.querySelector('img').src = originalImageSrc;
+	image7.querySelector('img').src = originalImageSrc;
+	image5.querySelector('img').src = originalImageSrc;
+	image6.querySelector('img').src = originalImageSrc;
+	image8.querySelector('img').src = originalImageSrc;
+	image9.querySelector('img').src = originalImageSrc;
+ });
+
+ image4.addEventListener('mouseover', () => {
+	image3.querySelector('img').src = 'myphot/IMAGE 2024-11-06 12:18:28.jpg';
+	image1.querySelector('img').src = 'myphot/IMAGE 2024-11-06 12:34:22.jpg';
+	image2.querySelector('img').src = 'myphot/IMAGE 2024-11-06 12:18:28.jpg';
+	image7.querySelector('img').src = 'myphot/IMAGE 2024-11-06 12:18:24.jpg';
+	image5.querySelector('img').src = 'myphot/IMAGE 2024-11-06 12:18:35.jpg';
+	image6.querySelector('img').src = 'myphot/IMAGE 2024-11-06 12:18:35.jpg';
+	image8.querySelector('img').src = 'myphot/IMAGE 2024-11-06 12:18:29.jpg';
+	image9.querySelector('img').src = 'myphot/IMAGE 2024-11-06 12:18:29.jpg';
+ });
+ image4.addEventListener('mouseout', () => {
+	image2.querySelector('img').src = originalImageSrc;
+	image1.querySelector('img').src = originalImageSrc;
+	image3.querySelector('img').src = originalImageSrc;
+	image4.querySelector('img').src = originalImageSrc;
+	image7.querySelector('img').src = originalImageSrc;
+	image5.querySelector('img').src = originalImageSrc;
+	image6.querySelector('img').src = originalImageSrc;
+	image8.querySelector('img').src = originalImageSrc;
+	image9.querySelector('img').src = originalImageSrc;
+ });
+ image5.addEventListener('mouseover', () => {
+	image3.querySelector('img').src = 'myphot/IMAGE 2024-11-06 12:18:28.jpg';
+	image4.querySelector('img').src = 'myphot/IMAGE 2024-11-06 12:18:31.jpg';
+	image1.querySelector('img').src = 'myphot/IMAGE 2024-11-06 12:18:22.jpg';
+	image2.querySelector('img').src = 'myphot/IMAGE 2024-11-06 12:34:22.jpg';
+	image7.querySelector('img').src = 'myphot/IMAGE 2024-11-06 12:18:25.jpg';
+	image6.querySelector('img').src = 'myphot/IMAGE 2024-11-06 12:18:35.jpg';
+	image8.querySelector('img').src = 'myphot/IMAGE 2024-11-06 12:18:24.jpg';
+	image9.querySelector('img').src = 'myphot/IMAGE 2024-11-06 12:18:29.jpg';
+ });
+ image5.addEventListener('mouseout', () => {
+	image2.querySelector('img').src = originalImageSrc;
+	image1.querySelector('img').src = originalImageSrc;
+	image3.querySelector('img').src = originalImageSrc;
+	image4.querySelector('img').src = originalImageSrc;
+	image7.querySelector('img').src = originalImageSrc;
+	image5.querySelector('img').src = originalImageSrc;
+	image6.querySelector('img').src = originalImageSrc;
+	image8.querySelector('img').src = originalImageSrc;
+	image9.querySelector('img').src = originalImageSrc;
+ });
+
+
+ image6.addEventListener('mouseover', () => {
+	image4.querySelector('img').src = 'myphot/IMAGE 2024-11-06 12:18:31.jpg';
+	image3.querySelector('img').src = 'myphot/IMAGE 2024-11-06 12:34:22.jpg';
+	image1.querySelector('img').src = 'myphot/IMAGE 2024-11-06 12:18:22.jpg';
+	image2.querySelector('img').src = 'myphot/IMAGE 2024-11-06 12:18:22.jpg';
+	image7.querySelector('img').src = 'myphot/IMAGE 2024-11-06 12:18:25.jpg';
+	image5.querySelector('img').src = 'myphot/IMAGE 2024-11-06 12:18:31.jpg';
+	image8.querySelector('img').src = 'myphot/IMAGE 2024-11-06 12:18:25.jpg';
+	image9.querySelector('img').src = 'myphot/IMAGE 2024-11-06 12:18:24.jpg';
+ });
+ image6.addEventListener('mouseout', () => {
+	image2.querySelector('img').src = originalImageSrc;
+	image1.querySelector('img').src = originalImageSrc;
+	image3.querySelector('img').src = originalImageSrc;
+	image4.querySelector('img').src = originalImageSrc;
+	image7.querySelector('img').src = originalImageSrc;
+	image5.querySelector('img').src = originalImageSrc;
+	image6.querySelector('img').src = originalImageSrc;
+	image8.querySelector('img').src = originalImageSrc;
+	image9.querySelector('img').src = originalImageSrc;
+ });
+
+ image7.addEventListener('mouseover', () => {
+	image4.querySelector('img').src = 'myphot/IMAGE 2024-11-06 12:34:22.jpg';
+	image3.querySelector('img').src = 'myphot/IMAGE 2024-11-06 12:18:28.jpg';
+	image1.querySelector('img').src = 'myphot/IMAGE 2024-11-06 12:34:22.jpg';
+	image2.querySelector('img').src = 'myphot/IMAGE 2024-11-06 12:18:28.jpg';
+	image5.querySelector('img').src = 'myphot/IMAGE 2024-11-06 12:18:28.jpg';
+	image6.querySelector('img').src = 'myphot/IMAGE 2024-11-06 12:18:28.jpg';
+	image8.querySelector('img').src = 'myphot/IMAGE 2024-11-06 12:18:35.jpg';
+	image9.querySelector('img').src = 'myphot/IMAGE 2024-11-06 12:18:35.jpg';
+ });
+ image7.addEventListener('mouseout', () => {
+	image2.querySelector('img').src = originalImageSrc;
+	image1.querySelector('img').src = originalImageSrc;
+	image3.querySelector('img').src = originalImageSrc;
+	image4.querySelector('img').src = originalImageSrc;
+	image7.querySelector('img').src = originalImageSrc;
+	image5.querySelector('img').src = originalImageSrc;
+	image6.querySelector('img').src = originalImageSrc;
+	image8.querySelector('img').src = originalImageSrc;
+	image9.querySelector('img').src = originalImageSrc;
+ });
+
+
+ image8.addEventListener('mouseover', () => {
+	image4.querySelector('img').src = 'myphot/IMAGE 2024-11-06 12:18:22.jpg';
+	image3.querySelector('img').src = 'myphot/IMAGE 2024-11-06 12:18:28.jpg';
+	image1.querySelector('img').src = 'myphot/IMAGE 2024-11-06 12:18:22.jpg';
+	image2.querySelector('img').src = 'myphot/IMAGE 2024-11-06 12:34:22.jpg';
+	image5.querySelector('img').src = 'myphot/IMAGE 2024-11-06 12:34:22.jpg';
+	image6.querySelector('img').src = 'myphot/IMAGE 2024-11-06 12:18:28.jpg';
+	image9.querySelector('img').src = 'myphot/IMAGE 2024-11-06 12:18:35.jpg';
+	image7.querySelector('img').src = 'myphot/IMAGE 2024-11-06 12:18:31.jpg';
+ });
+ image8.addEventListener('mouseout', () => {
+	image2.querySelector('img').src = originalImageSrc;
+	image1.querySelector('img').src = originalImageSrc;
+	image3.querySelector('img').src = originalImageSrc;
+	image4.querySelector('img').src = originalImageSrc;
+	image7.querySelector('img').src = originalImageSrc;
+	image5.querySelector('img').src = originalImageSrc;
+	image6.querySelector('img').src = originalImageSrc;
+	image8.querySelector('img').src = originalImageSrc;
+	image9.querySelector('img').src = originalImageSrc;
+	
+ });
+
+ image9.addEventListener('mouseover', () => {
+	image5.querySelector('img').src = 'myphot/IMAGE 2024-11-06 12:18:22.jpg';
+	image3.querySelector('img').src = 'myphot/IMAGE 2024-11-06 12:34:22.jpg';
+	image1.querySelector('img').src = 'myphot/IMAGE 2024-11-06 12:18:22.jpg';
+	image2.querySelector('img').src = 'myphot/IMAGE 2024-11-06 12:18:22.jpg';
+	image4.querySelector('img').src = 'myphot/IMAGE 2024-11-06 12:18:22.jpg';
+	image8.querySelector('img').src = 'myphot/IMAGE 2024-11-06 12:18:31.jpg';
+	image7.querySelector('img').src = 'myphot/IMAGE 2024-11-06 12:18:31.jpg';
+	image6.querySelector('img').src = 'myphot/IMAGE 2024-11-06 12:34:22.jpg';
+ });
+ image9.addEventListener('mouseout', () => {
+	image2.querySelector('img').src = originalImageSrc;
+	image1.querySelector('img').src = originalImageSrc;
+	image3.querySelector('img').src = originalImageSrc;
+	image4.querySelector('img').src = originalImageSrc;
+	image7.querySelector('img').src = originalImageSrc;
+	image5.querySelector('img').src = originalImageSrc;
+	image6.querySelector('img').src = originalImageSrc;
+	image8.querySelector('img').src = originalImageSrc;
+	image9.querySelector('img').src = originalImageSrc;
+ });
